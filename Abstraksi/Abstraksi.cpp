2ba17c796b62313f7f40ab5374cf4610a4ef33c1
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-28 11:11:05 +0700

    menambahkan program inti abstraksi

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -14,13 +14,16 @@ public:
     //menampilkan nilai
     void display() {
         cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+
     }
 };
 
-
-
-
 int main()
 {
-    
+    AbstraksiKlas ak;
+    ak.setXY("Yogyakarta", "Kampus");
+    ak.display();
+
+    return 0;
 }
