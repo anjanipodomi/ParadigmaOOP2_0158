b801556d568d2311e23632e78a7ba7c48dfe564a
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-28 10:18:32 +0700

    membuat akses modifier private dan pure virtual function

---
+++ Paradigmaoop2/Paradigmaoop2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class seseorang
+{
+public:
+	//pure virtual function
+	virtual void pesan() = 0;
+
+	//virtual function biasa
+	//virtual void pesan()
+	//{
+	//cout<< "pesan dari seseorang" << endl;
+	//}
+};
+
+int main()
+{
+   
+}
