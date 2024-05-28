835feb587ee04a14a17a41778e27908c20b2aad7
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-28 11:40:17 +0700

    membuat solve multiwaris, sama dengan projek multiwaris hanya dihapus virtual dan orang pada bagian class budi

---
+++ solveMultiwaris/solveMultiwaris.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class orang
+{
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
+
+class Pekerja : public orang
+{
+public:
+
+    Pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang
+{
+public:
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
+class budi : public Pekerja, public pelajar
+{
+public:
+    budi(int pUmur) :
+        Pekerja(pUmur),
+        pelajar(pUmur)
+    {
+        cout << "Budi dibuat/n" << endl;
+    }
+};
+
+int main()
+{
+    budi a(12);
+
+    return 0;
+}
