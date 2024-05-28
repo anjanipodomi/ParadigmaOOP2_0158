8a3067b1a43075aa3d9d351f80b857415f4fec91
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-28 10:31:51 +0700

    membuat class baseClass dengan akses public

---
+++ noOverriding/noOverriding.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+//class parent
+//tambahkan final sesudah nama class
+//untuk mencegah adanya overriding
+
+class baseClass
+{
+public:
+
+};
+
+
+
+
+
+int main()
+{
+   
+}
