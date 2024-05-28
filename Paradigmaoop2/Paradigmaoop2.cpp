9d375c834c28a327c1a4e19d456c9f51dd22a368
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-28 10:28:46 +0700

    menambahkan program inti

--- Paradigmaoop2/Paradigmaoop2.cpp
+++ Paradigmaoop2/Paradigmaoop2.cpp
@@ -34,9 +34,20 @@ public:
 	}
 };
 
-
-
 int main()
 {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+
+	//akses method pesan() dari seseorang
+	//a.seseorang::pesan();
+
+	return 0;
    
 }
